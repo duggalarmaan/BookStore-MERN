9faa21b08a639e5f64d7e1e6d4bdde51ae603441
duggalarmaan <72279535+duggalarmaan@users.noreply.github.com>
2024-04-19 13:57:38 -0600

    updated vercel.json

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,44 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
 import cors from 'cors';
-import serverless from 'serverless-http';
 
 const app = express();
 
-// Middleware for parsing JSON request body
+const PORT = 3000;
+
+//Midleware for parsing json request body
 app.use(express.json());
 
-// Middleware for handling CORS Policy
+//Middleware for handling CORS Policy
+//Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
 
-// Root route
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World!' });
+//Option 2: Allow Custom Origins
+/*app.use(
+    cors({
+        origin: 'https://book-store-mern-three.vercel.app',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    })
+);*/
+
+app.get('/', (request,response) => {
+    console.log(request)
+    return response.json('Hello World');
 });
 
-// Books route
 app.use('/books', booksRoute);
 
-// Connect to MongoDB
-const connectToDatabase = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://root:root@book-store-mern.q2pfp2w.mongodb.net/books-collection?retryWrites=true&w=majority');
-        console.log('App connected to database');
-    } catch (error) {
-        console.error('Database connection error:', error);
-    }
-};
+mongoose
+.connect('mongodb+srv://root:root@book-store-mern.q2pfp2w.mongodb.net/books-collection?retryWrites=true&w=majority')
+.then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });       
+})
+.catch((error) => {
+    console.log(error);
+});
 
-// Invoke database connection
-connectToDatabase();
-
-// Export the app as a serverless handler
-export default serverless(app);
+export default app;
