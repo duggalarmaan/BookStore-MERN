d426d5aad27c547f0f7745eadc71fee834336c20
duggalarmaan <72279535+duggalarmaan@users.noreply.github.com>
2024-04-19 10:07:24 -0600

    Updated import on index.js for server.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { request, response } from "express"
+import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose, { mongo } from "mongoose";
 import { Book } from "./models/bookModel.js";
